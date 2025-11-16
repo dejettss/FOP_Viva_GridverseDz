daa7a92655c88388826983ec9568eae49ae976cc
Nazeef Nuwaisir Khan <nazeef05khan@gmail.com>
2025-11-16 19:51:02 +0800

    Improve input validation for inquiries and H value
    Refactor input validation for inquiries and number range.

--- Viva1/V01Q03.java
+++ Viva1/V01Q03.java
@@ -4,16 +4,16 @@ public class V01Q03 {
         Scanner input = new Scanner(System.in);
         
         int T = input.nextInt();
-        //The number of inquiries, T, will be between 1 and 50. 
-        if(T>=1 && T<=10){ 
+
+        
         for(int d=1; d<=T;d++){
              
 
         int H = input.nextInt();
 
         String S = input.next();
-        //the number of H will be 1-9 or else its invalid
-        if(H>=1&&H<=9){
+
+
         if(S.equals("A")){
             for(int i=1;i<=H;i++){
                 for(int u=1;u<=i;u++){
@@ -34,8 +34,8 @@ public class V01Q03 {
                     System.out.print(r);
                 }System.out.println();
             }
-        }}
-    }
+        }
+    
     }
     }    
 }
